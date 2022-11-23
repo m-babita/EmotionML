e2e267f8b411ecd9989e31180a5a408a23132f7e
m-babita <babita.majumdar.1@gmail.com>
2022-11-23 20:44:55 +0530

    public app

--- app.py
+++ app.py
@@ -20,8 +20,7 @@ def video():
     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(debug=False,host='0.0.0.0')
 
 
 # import os
